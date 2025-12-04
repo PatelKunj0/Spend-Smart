1ef6f2bf8f8456db263dbdf488b8ef938ce7c20f
Kunj Patel <patelkunj@csu.fullerton.edu>
2025-12-03 16:18:49 -0800

    Initial Commit

---
+++ Spend Smart/Spend_SmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_SmartApp.swift
+//  Spend Smart
+//
+//  Created by Kunj Patel on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Spend_SmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spend SmartTests/Spend_SmartTests.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_SmartTests.swift
+//  Spend SmartTests
+//
+//  Created by Kunj Patel on 12/3/25.
+//
+
+import Testing
+@testable import Spend_Smart
+
+struct Spend_SmartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
